848b9d6b1bf5837254b397af55f11634123c48b4
beamimc <beatrizcampillo29@gmail.com>
2025-06-14 21:50:41 -0400

    fix data rel path

--- run_app.R
+++ run_app.R
@@ -1,15 +1,20 @@
-# launch_app.R
+# launch app.R
+# This script is used to launch a Shiny app with the required data files.
+
 library(here)
-# library(GenomicFeatures)
+library(shiny)
 
+# Ensure the working directory is set to the project root
+setwd(here::here()) 
 
-see <- readRDS(here::here("data","new", "glinos_saturn_dtu.rds"))
-# txdbb <- loadDb(here("data","flair_filter_transcripts.sqlite"))
-exonss <- readRDS(here::here("data", "new", "glinos_exons.rds"))
+# Load the required data files
+see <- readRDS(here::here("data", "glinos_saturn_dtu.rds"))
+exonss <- readRDS(here::here("data", "glinos_exons.rds"))
 
-
+# Load the app.R file from the app directory
 app_dir <-file.path(here::here(), "app")
 source(file.path(app_dir, "app.R"))
+# Run the Shiny app with the provided data
 shiny::runApp(app(se= see, 
                   exons=exonss, 
                   app_dir = app_dir))
