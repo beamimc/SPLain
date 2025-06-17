1625ccd071900caf8da0e45b5ac2aaa872a0f7ec
Justin Landis <justinlandis@Justins-MacBook-Air.local>
2025-06-17 16:06:09 -0400

    shiny is now required

---
+++ R/SPLain-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment colData<-
+#' @importFrom SummarizedExperiment rowData
+#' @importFrom SummarizedExperiment rowData<-
+## usethis namespace: end
+NULL
