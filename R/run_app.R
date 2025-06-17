a92ad7a2d12f3a2cf11d4258940b2b48640d0f0e
Justin Landis <justinlandis@Justins-MacBook-Air.local>
2025-06-17 16:03:56 -0400

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,21 +1,20 @@
 # launch app.R
 # This script is used to launch a Shiny app with the required data files.
 
-library(here)
-library(shiny)
+# library(here)
+# library(shiny)
 
-# Ensure the working directory is set to the project root
-setwd(here::here()) 
+# # Ensure the working directory is set to the project root
+# setwd(here::here())
 
-# Load the required data files
-see <- readRDS(here::here("data", "glinos_saturn_dtu.rds"))
-exonss <- readRDS(here::here("data", "glinos_exons.rds"))
+# # Load the required data files
+# see <- readRDS(here::here("data", "glinos_saturn_dtu.rds"))
+# exonss <- readRDS(here::here("data", "glinos_exons.rds"))
 
 # Load the app.R file from the app directory
 app_dir <-file.path(here::here(), "app")
 source(file.path(app_dir, "app.R"))
 # Run the Shiny app with the provided data
-shiny::runApp(app(se= see, 
-                  exons=exonss, 
+shiny::runApp(app(se= see,
+                  exons=exonss,
                   app_dir = app_dir))
-
