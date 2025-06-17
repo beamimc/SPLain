879099dcf34794a5cabb6fbeae3d39d0b6d868d3
Justin Landis <justinlandis@Justins-MacBook-Air.local>
2025-06-17 17:29:38 -0400

    Adding imports for a bunch of stuff

--- R/SPLain-package.R
+++ R/SPLain-package.R
@@ -2,10 +2,32 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom BSgenome.Hsapiens.UCSC.hg38 Hsapiens
 #' @importFrom dplyr %>%
+#' @importFrom dplyr bind_cols
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr desc
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr row_number
+#' @importFrom dplyr summarise
+#' @importFrom dplyr summarize
+#' @importFrom org.Hs.eg.db org.Hs.eg.db
+#' @importFrom plyranges filter_by_non_overlaps_directed
+#' @importFrom SummarizedExperiment assay
+#' @importFrom SummarizedExperiment assay<-
+#' @importFrom SummarizedExperiment assays
+#' @importFrom SummarizedExperiment assays<-
 #' @importFrom SummarizedExperiment colData
 #' @importFrom SummarizedExperiment colData<-
+#' @importFrom SummarizedExperiment mcols
+#' @importFrom SummarizedExperiment mcols<-
 #' @importFrom SummarizedExperiment rowData
 #' @importFrom SummarizedExperiment rowData<-
+#' @importFrom tibble tibble
+#' @importFrom tidyr pivot_longer
 ## usethis namespace: end
 NULL
